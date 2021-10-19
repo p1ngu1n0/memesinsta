1530f1e862e22d61ae885ec318fe6de8a802fe26
p1ngu1n0 <36717227+p1ngu1n0@users.noreply.github.com>
2021-10-19 14:24:10 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # -*- coding=utf-8 -*-
 
 import os
+import PIL
 import time
 import random
 import settings
@@ -13,7 +14,7 @@ from PIL import Image
 class my_bot():
     def __init__(self):
         self.tmpbot = instabot.Bot()
-        self.tmpbot.login(username=settings.user, password=settings.password)
+        self.tmpbot.login(username="user", password="pass")
 
     def download_memes(self, hashtag: str, count: int):
         loot = instalooter.looters.HashtagLooter(hashtag)
@@ -25,7 +26,10 @@ class my_bot():
 
     def create_meme(self, image: str):
         marcadeagua = Image.open("marcadeagua.png")
-        meme = Image.open(f"{settings.directory}\\{image}")
+        try:
+            meme = Image.open(f"{settings.directory}//{image}")
+        except PIL.UnidentifiedImageError:
+            pass
         x, y = meme.size
         size = 60
         meme.paste(marcadeagua.resize((settings.smarca, settings.smarca)),
@@ -33,18 +37,45 @@ class my_bot():
         meme.save("meme.jpg")
 
     def upload_memes(self):
+        self.tmpbot.logger.info(
+            f"Fotos por subir {len(os.listdir(settings.directory))}")
+        self.tmpbot.logger.info(
+            f"Dias por foto {len(os.listdir(settings.directory))/50}")
         for x in os.listdir(settings.directory):
-            self.create_meme(x)
-            self.tmpbot.upload_photo(f"meme.jpg", caption=settings.comentario)
-            time.sleep(settings.delay)
-            os.remove(f"meme.jpg.REMOVE_ME")
-            os.remove(f"{settings.directory}\\{x}")
+            if x[-4:] == ".mp4":
+                try:
+                    self.tmpbot.upload_video(
+                        f"{settings.directory}//{x}", f"{random.choice(settings.opinion)}\n\n----------ignorar--------\n{random.choice(settings.hashtags)}")
+                    #os.remove(f"{settings.directory}//{x}")
+                    #os.remove(f"{settings.directory}//{x}.jpg")
+                    #os.remove(f"{settings.directory}//{x}.CONVERTED.mp4.REMOVE_ME")
+                except Exception:
+                    continue
+            else:
+                self.create_meme(x)
+                self.tmpbot.upload_photo(
+                    f"meme.jpg", caption=f"{random.choice(settings.opinion)}\n\n----------ignorar--------\n{random.choice(settings.hashtags)}")
+                os.remove(f"{settings.directory}//{x}")
+                try:
+                    os.remove(f"meme.jpg.REMOVE_ME")
+                except FileNotFoundError:
+                    self.upload_memes()
+            print("Esperando una hora")
+            time.sleep(10)
+
+    def follow_persons(self):
+        #[bot.get_username_from_user_id(x) for x in bot.get_user_followers("adrian_dct")]
+        for x in range(12):
+            self.tmpbot.follow(self.usuarios[x])
+            self.tmpbot.logger.info(
+                f"EL bot acaba de seguir a {self.tmpbot.get_username_from_user_id(self.usuarios[x])}")
+            self.usuarios.pop(x)
 
 
 def main():
     bot = my_bot()
     while True:
-        bot.download_memes(settings.hashtag, settings.cout_images)
+        print("[ ok ] Pulsa Ctrl + C para salir ")
         bot.upload_memes()
 
 
@@ -53,4 +84,4 @@ if __name__ == "__main__":
         main()
     except KeyboardInterrupt:
         print("Hastal la proxima :D")
-        
+        os.remove("config")
--- settings.py
+++ settings.py
@@ -1,24 +1,12 @@
-import os
-import random
-user = ""
-password = ""
-memeporhora = 3
 marcadeagua = "marcadeagua.png"
-delay = 3600 / memeporhora
 hashtag = "shitpostespañol"
 cout_images = 72
 smarca = 80
 directory = "memes"
-opinion = ["Sigueme para mas contenido en @macatangas"]
+opinion = ["Sigueme para mas contenido en @mascatangas", "@mascatangas la mejor cuenta de momazos"]
 hashtags = ["#dankmemes #dankmemescantmeltsteelbeams #dankmemescanmeltsteelbeams #dankmemesdaily #dankmemesmatter #dankmemes2k17 #dankmemesig #dankmemesmeltsteelbeams #dankmemes2k16 #dankmemescanmeltstealbeams #dankmemesdontmeltsteelbeams #dankmemescantmeltstealbeams #dankmemesaredank #dankmemesociety #dankmemesgang #dankmemescaneltstealbeams #dankmemescantmeltsleelbeams #dankmemescantmeltsteambeams #dankmemesarethefuture #dankmemesarelife #dankmemeshavedreams #dankmemes4free #dankmemescanmeltironbeam #dankmemes2017 #dankmemesita #dankmemesfordays #dankmemesfordankdreams #dankmemesonly #dankmemesvideo #dankmemes0 ",
             "#memesdaily #memestagram #memesfordays #memesita #memesaremee #memesespa #memestar #memesenespa #memester #memes4ever #memesbelike #memestealer #memeslayer #memesforlife #memesrlife #memesitalia #memes4life #memesarelife #memesdank #memes2k17 #memeslut #MemesUniversit #memes2good #memesftw #memesandbeauty #memeslivesmatter #memesdailybestmemes #memesofig #memesilikonu #memesrus ",
             "#shitpost #shitposting #shitposts #shitpostsunday #shitposter #shitpostdaily #shitposthttps #shitpostaesthetic #shitposters #shitpostingtrash #shitpostsgenerator #shitpostaccount #shitpostdoggo #shitpostallday #shitpostingitalia #shitpostingtime #shitpostmeme ",
             "#Spanish #spanishgirl #spanishfood #spanishboy #spanishmodel #spanishhorse #spanishblogger #spanishmusic #spanishgirls #spanishwine #spanishsteps #spanishguitar #SpanishTrap #Spanishharlem #spanishboys #spanishstyle #spanishmoss #spanishcuisine #spanishart #spanishlessons #SpanishGP #spanisharchitecture #spanishwomen #spanishbrand #spanishcolonial #spanishgreyhound #spanishrice #SpanishQuotes #spanishartist #Spanishlife"
             ]
-comentario = f"""
-{random.choice(opinion)}
-
-
-----------ignorar--------
-{random.choice(hashtags)}
-"""
+#comentario = f"{random.choice(opinion)}\n\n----------ignorar--------\n{random.choice(hashtags)}"
